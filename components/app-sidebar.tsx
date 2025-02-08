269c6f6136905865f2ea72e1eb0be4adbfd8d1da
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-02-08 12:13:58 +0500

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Search } from "lucide-react"
 import { UserButton } from "@clerk/nextjs"
 import {
   Sidebar,
